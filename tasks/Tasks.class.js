5f59b574d6b4bb88181e819080f62c1ee7ec0824
dfm555 <dfm556@gmail.com>
2022-08-10 21:14:28 -0500

    Add Tasks Class

---
+++ tasks/Tasks.class.js
@@ -0,0 +1,33 @@
+class Tasks {
+  taskList = [];
+
+  getTasks() {
+    return this.taskList;
+  }
+
+  getTask(id) {
+    return this.taskList.find((task) => task.id === id);
+  }
+
+  addTask(task) {
+    //Code here
+    if (task) {
+      this.taskList.push({
+        id: this.taskList.length, // Tamaño del array
+        activity: task,
+        done: false,
+      }); // agregar tarea a la colección de tareas
+    }
+  }
+
+  editTask(id, value) {
+    const index = this.taskList.findIndex((value) => {
+      return value.id === id;
+    });
+    this.taskList[index].activity = value;
+  }
+
+  deleteTask(id) {
+    this.taskList = this.taskList.filter((task) => task.id !== id);
+  }
+}
